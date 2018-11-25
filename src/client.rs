6fff871d7386260c1f5809beeabe250c446d4e6c
Simon Walker <s.r.walker101@googlemail.com>
2018-11-25 21:44:23 +0000

    Include connection_config object

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,12 @@
 use commands::Command;
+use connection_config::ConnectionConfig;
 use errors::Result;
+use failure::format_err;
+use hmac::Mac;
+use log::trace;
 use responses::Response;
 use signatures::HmacSha256;
+use std::io::Read;
 
 use socket::Socket;
 
@@ -11,6 +16,35 @@ pub struct Client {
 }
 
 impl Client {
+    pub fn from_reader<R>(reader: R) -> Result<Self>
+    where
+        R: Read,
+    {
+        let config: ConnectionConfig = serde_json::from_reader(reader)?;
+        let auth = HmacSha256::new_varkey(config.key.as_bytes())
+            .map_err(|e| format_err!("Error constructing HMAC: {:?}", e))?;
+
+        let ctx = zmq::Context::new();
+
+        // Set up the sockets
+        trace!("shell port: {}", config.shell_port);
+        let shell_socket = ctx.socket(zmq::REQ)?;
+        shell_socket.connect(&format!("tcp://localhost:{port}", port = config.shell_port))?;
+
+        // Set up iopub socket
+        /*
+        trace!("iopub port: {}", config.iopub_port);
+        let iopub_socket = ctx.socket(zmq::SUB)?;
+        iopub_socket.connect(&format!("tcp://localhost:{port}", port = config.iopub_port))?;
+        iopub_socket.set_subscribe("".as_bytes())?;
+        */
+
+        Ok(Client {
+            shell_socket: Socket(shell_socket),
+            auth: auth,
+        })
+    }
+
     pub fn send_shell_command(&self, command: Command) -> Result<Response> {
         let wire = command.into_wire(self.auth.clone())?;
         self.shell_socket.send_wire(wire)?;
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 extern crate failure;
 extern crate hex;
 extern crate hmac;
+extern crate log;
+extern crate serde;
+extern crate serde_derive;
 extern crate sha2;
 extern crate zmq;
 
 mod client;
 mod commands;
+mod connection_config;
 mod errors;
 mod responses;
 mod signatures;
