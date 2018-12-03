58cfa0ff40e7919343692fb6a71d458fbb7b63bc
Simon Walker <s.r.walker101@googlemail.com>
2018-12-03 21:39:38 +0000

    Restart example takes command line arguments

--- examples/shutdown.rs
+++ examples/shutdown.rs
@@ -1,25 +1,33 @@
 extern crate env_logger;
 extern crate jupyter_client;
+extern crate structopt;
 
 use jupyter_client::commands::Command;
 use jupyter_client::Client;
 use std::thread;
 use std::time::Duration;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(long = "restart", short = "r")]
+    /// Restart the server
+    restart: bool,
+}
 
 fn main() {
     env_logger::init();
 
+    let args = Opt::from_args();
+
     let client = Client::existing().expect("creating jupyter connection");
 
-    let receiver = client.iopub_subscribe().unwrap();
-    thread::spawn(move || {
-        for msg in receiver {
-            println!("{:?}", msg);
-        }
-    });
-
-    let command = Command::Shutdown { restart: false };
-    let response = client.send_shell_command(command).expect("sending command");
+    let command = Command::Shutdown {
+        restart: args.restart,
+    };
+    let response = client
+        .send_control_command(command)
+        .expect("sending command");
     println!("Response: {:#?}", response);
 
     thread::sleep(Duration::from_secs(1));
